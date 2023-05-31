6c549e4405ecd642ef20f18fb0433b5b32c8c7fb
mcbashie <59084178+mcbashie@users.noreply.github.com>
2023-05-31 09:47:46 +0200

    3 files modified, 1 added, 1 deleted

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
   standalone: true,
   imports: [CommonModule],
   template: `
-  <app-select title="ATW art 2.5" [disabled]="!isOpen" [options]="calamityOptions" [selectedOption]=""
+  <app-select title="Title" [disabled]="!isOpen" [options]="calamityOptions" [selectedOption]=""
   (selectionChange)="selectedCalamityChanged($event)" [showSearchField]="false">
   </app-select>
   `,
@@ -21,27 +21,32 @@ export class App implements OnInit {
 
   constructor() {
     this.isOpen = false;
-    this.calamityOptions.unshift({
-      viewValue: 'A',
-      value: 'A',
-      valueCode: 'A',
-    });
 
-    this.calamityOptions.unshift({
-      viewValue: 'B',
-      value: 'B',
-      valueCode: 'B',
-    });
-
-    this.calamityOptions.unshift({
-      viewValue: '-',
-      value: '',
-      valueCode: '',
-    });
   }
 
   selectedCalamityChanged(event): void {
     this.chosenOption = event;
+  }
+
+  ngOnInit(): void {
+
+  this.calamityOptions.unshift({
+    viewValue: 'A',
+    value: 'A',
+    valueCode: 'A',
+  });
+
+  this.calamityOptions.unshift({
+    viewValue: 'B',
+    value: 'B',
+    valueCode: 'B',
+  });
+
+  this.calamityOptions.unshift({
+    viewValue: '-',
+    value: '',
+    valueCode: '',
+  });
   }
 }
 
