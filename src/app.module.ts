61060823833c0611b4d002546beee9e92022a69a
mcbashie <59084178+mcbashie@users.noreply.github.com>
2023-05-31 13:54:37 +0200

    1 file modified, 1 added

---
+++ src/app.module.ts
@@ -0,0 +1,55 @@
+import { SelectComponent } from './select.component';
+import { MatLegacyButtonModule as MatButtonModule } from '@angular/material/legacy-button';
+import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
+import { MatLegacyCheckboxModule as MatCheckboxModule } from '@angular/material/legacy-checkbox';
+import { MatLegacyChipsModule as MatChipsModule } from '@angular/material/legacy-chips';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatLegacyDialogModule as MatDialogModule } from '@angular/material/legacy-dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatLegacyFormFieldModule as MatFormFieldModule } from '@angular/material/legacy-form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
+import { MatLegacyMenuModule as MatMenuModule } from '@angular/material/legacy-menu';
+import { MatLegacyProgressSpinnerModule as MatProgressSpinnerModule } from '@angular/material/legacy-progress-spinner';
+import { MatLegacyRadioModule as MatRadioModule } from '@angular/material/legacy-radio';
+import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
+import { MatLegacySlideToggleModule as MatSlideToggleModule } from '@angular/material/legacy-slide-toggle';
+import { MatLegacySnackBarModule as MatSnackBarModule } from '@angular/material/legacy-snack-bar';
+import { MatLegacyTableModule as MatTableModule } from '@angular/material/legacy-table';
+import { MatLegacyTabsModule as MatTabsModule } from '@angular/material/legacy-tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SelectComponent } from './select.component';
+
+
+@NgModule({
+  declarations: [ 
+    SelectComponent
+  ],
+ imports: [
+   MatButtonModule,
+   MatCardModule,
+   MatCheckboxModule,
+   MatChipsModule,
+   MatDatepickerModule,
+   MatDialogModule,
+   MatExpansionModule,
+   MatFormFieldModule,
+   MatIconModule,
+   MatInputModule,
+   MatListModule,
+   MatMenuModule,
+   MatProgressSpinnerModule,
+   MatSelectModule,
+   MatSlideToggleModule,
+   MatTableModule,
+   MatTabsModule,
+   MatToolbarModule,
+   MatSnackBarModule,
+   MatRadioModule,
+ ],
+ providers: [],
+ bootstrap: [AppComponent] });
+
+ export class AppModule {}
+
